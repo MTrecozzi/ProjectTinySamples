0cb98789333775feeb48f39a0ba3452d536c4870
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2020-03-11 15:06:56 -0400

    Dots Content Authoring needs some work

---
+++ TinySpaceship/Assets/TinySpaceship/Scripts/Authoring/PlayerMovementAuthoring.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovementAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponent<PlayerMovement>(entity);
+    }
+}
+
+public struct PlayerMovement : IComponentData
+{
+    public float2 direction;
+}
---
+++ TinySpaceship/Assets/TinySpaceship/Scripts/Runtime/PlayerMovementSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.U2D.Entities.Physics;
+
+//using Unity.U2D.Entities.Physics;
+
+public class PlayerMovementSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        var physicsWorldSystem = World.GetExistingSystem<PhysicsWorldSystem>();
+        var physicsWorld = physicsWorldSystem.PhysicsWorld;
+
+        Entities.WithAll<PlayerMovement>().ForEach((Entity playerEntity, ref PhysicsColliderBlob velocity ) =>
+            {
+
+                {
+                    
+                }
+                
+            }
+            
+            );
+    }
+}
